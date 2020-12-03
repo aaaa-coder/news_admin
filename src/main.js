54c9cd54b879f252b98650e13386666d531c0cfa
tangchaofu <1027402361@qq.com>
2020-12-03 11:38:59 +0800

    'axios与emement初始化'

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+import axios from "axios";
+Vue.prototype.$axios = axios;
+axios.defaults.baseURL = 'http://liangwei.tech:3000';
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 
 new Vue({
   router,
