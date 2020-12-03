e0ec5be788a29164f7b1a263f3d1ee6c3feb92d9
tangchaofu <1027402361@qq.com>
2020-12-03 15:46:19 +0800

    '新建登录页面，完成登录功能'

---
+++ src/views/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <el-row type="flex" justify="center" align="middle" class="container">
+    <el-form ref="form" label-width="80px">
+      <el-form-item label="用户名">
+        <el-input v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.password" type="password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">登录</el-button>
+        <el-button>重置</el-button>
+      </el-form-item>
+    </el-form>
+  </el-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$axios({
+        method: "post",
+        url: "/login",
+        data: this.form,
+      }).then((res) => {
+        if (res.status === 200) {
+          const { data } = res.data;
+          localStorage.setItem("token", data.token);
+          this.$message.success("登录成功");
+        }
+      });
+    },
+  },
+};
+</script>
+<style lang="less" scoped>
+.container {
+  height: 100vh;
+}
+</style>
